da428ffcb3fdb65ba25a64ad2958a6f0095e92f1
Amelia Clarke <ameliavoncat@gmail.com>
2017-04-28 23:54:53 -0700

    files can be opened and viewed in the editor

--- src/output/editorDrawRows.c
+++ src/output/editorDrawRows.c
@@ -1,21 +1,27 @@
 void editorDrawRows(struct abuf *ab) {
   int y;
   for (y = 0; y < E.screenrows; y++) {
-    if(y == E.screenrows / 3) {
-      char welcome[80];
-      int welcomelen = snprintf(
-        welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION
-      );
-      if(welcomelen > E.screencols) welcomelen = E.screencols;
-      int padding = (E.screencols - welcomelen) / 2;
-      if (padding) {
+    if (y >= E.numrows) {
+      if (E.numrows == 0 && y == E.screenrows / 3) {
+        char welcome[80];
+        int welcomelen = snprintf(
+          welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION
+        );
+        if(welcomelen > E.screencols) welcomelen = E.screencols;
+        int padding = (E.screencols - welcomelen) / 2;
+        if (padding) {
+          abAppend(ab, "~", 1);
+          padding--;
+        }
+        while (padding--) abAppend(ab, " ", 1);
+        abAppend(ab, welcome, welcomelen);
+      } else {
         abAppend(ab, "~", 1);
-        padding--;
       }
-      while (padding--) abAppend(ab, " ", 1);
-      abAppend(ab, welcome, welcomelen);
     } else {
-      abAppend(ab, "~", 1);
+      int len = E.row[y].size;
+      if (len > E.screencols) len = E.screencols;
+      abAppend(ab, E.row[y].chars, len);
     }
 
     abAppend(ab, "\x1b[K", 3);
