4678572c8e048eb7a860a038185540ac65d59fa7
Amelia Clarke <ameliavoncat@gmail.com>
2017-04-29 00:31:40 -0700

    adds horizontal scrolling

--- src/output/editor_scroll.c
+++ src/output/editor_scroll.c
@@ -5,4 +5,10 @@ void editorScroll() {
   if (E.cy > E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
+  if (E.cx < E.coloff) {
+    E.coloff = E.cx;
+  }
+  if (E.cx >= E.coloff + E.screencols) {
+    E.coloff = E.cx - E.screencols + 1;
+  }
 }
