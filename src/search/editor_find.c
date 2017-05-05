d8a33582bd20d7d31cf10a4362657a584f507bdd
Amelia Clarke <ameliavoncat@gmail.com>
2017-05-04 19:18:47 -0700

    allows the user to escape from search to previous cursor position

--- src/search/editor_find.c
+++ src/search/editor_find.c
@@ -1,7 +1,17 @@
 void editorFind() {
+  int saved_cx = Editor.char_position_x;
+  int saved_cy = Editor.char_position_y;
+  int saved_coloff = Editor.col_offset;
+  int saved_rowoff = Editor.row_offset;
+
   char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
 
   if (query) {
     free(query);
+  } else {
+    Editor.char_position_x = saved_cx;
+    Editor.char_position_y = saved_cy;
+    Editor.col_offset = saved_coloff;
+    Editor.row_offset = saved_rowoff;
   }
 }
