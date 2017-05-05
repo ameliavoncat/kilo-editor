85fb90c362a42d7a5c35bb49a205fdbda2035973
Amelia Clarke <ameliavoncat@gmail.com>
2017-05-04 19:15:05 -0700

    adds incremental search

--- src/search/editor_find.c
+++ src/search/editor_find.c
@@ -1,18 +1,7 @@
 void editorFind() {
-  char *query = editorPrompt("Search: %s (ESC to cancel)", NULL);
-  if (query == NULL) return;
+  char *query = editorPrompt("Search: %s (ESC to cancel)", editorFindCallback);
 
-  int i;
-  for (i = 0; i < Editor.numrows; i++) {
-    erow *row = &Editor.row[i];
-    char *match = strstr(row->render, query);
-    if (match) {
-      Editor.char_position_y = i;
-      Editor.char_position_x = editorRowRxToCx(row, match - row->render);
-      Editor.row_offset = Editor.numrows;
-      break;
-    }
+  if (query) {
+    free(query);
   }
-
-  free(query);
 }
