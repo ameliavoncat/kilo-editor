db6a5e0cfae322f8ee032678cdf014d9fffe5d16
Amelia Clarke <ameliavoncat@gmail.com>
2017-05-08 13:16:10 -0700

    adds keyword highlighting and defines keywords and types in c

--- src/data/editor_syntax.c
+++ src/data/editor_syntax.c
@@ -1,6 +1,7 @@
 struct editorSyntax {
   char *filetype;
   char **filematch;
+  char **keywords;
   char *singleline_comment_start;
   int flags;
 };
--- src/syntax_highlighting/editor_syntax_to_color.c
+++ src/syntax_highlighting/editor_syntax_to_color.c
@@ -1,6 +1,8 @@
 int editorSyntaxToColor(int hl) {
   switch(hl) {
     case HL_COMMENT: return 36;
+    case HL_KEYWORD1: return 33;
+    case HL_KEYWORD2: return 32;
     case HL_STRING: return 35;
     case HL_NUMBER: return 31;
     case HL_MATCH: return 34;
