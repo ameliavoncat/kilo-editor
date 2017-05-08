8ef2f43446488dbd06e839d496f8419df7409017
Amelia Clarke <ameliavoncat@gmail.com>
2017-05-08 13:05:52 -0700

    adds comment highlighting for c files

--- src/syntax_highlighting/editor_syntax_to_color.c
+++ src/syntax_highlighting/editor_syntax_to_color.c
@@ -1,5 +1,6 @@
 int editorSyntaxToColor(int hl) {
   switch(hl) {
+    case HL_COMMENT: return 36;
     case HL_STRING: return 35;
     case HL_NUMBER: return 31;
     case HL_MATCH: return 34;
--- src/syntax_highlighting/editor_update_syntax.c
+++ src/syntax_highlighting/editor_update_syntax.c
@@ -4,6 +4,9 @@ void editorUpdateSyntax(erow *row) {
 
   if (Editor.syntax == NULL) return;
 
+  char *scs = Editor.syntax->singleline_comment_start;
+  int scs_len = scs ? strlen(scs) : 0;
+
   int prev_sep = 1;
   int in_string = 0;
 
@@ -11,6 +14,13 @@ void editorUpdateSyntax(erow *row) {
   while (i < row->rsize) {
     char c = row->render[i];
     unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
+
+    if (scs_len && !in_string) {
+      if (!strncmp(&row->render[i], scs, scs_len)) {
+        memset(&row->hl[i], HL_COMMENT, row->rsize - i);
+        break;
+      }
+    }
 
     if (Editor.syntax->flags & HL_HIGHLIGHT_STRINGS) {
       if (in_string) {
